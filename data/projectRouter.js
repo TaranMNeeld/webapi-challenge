8fab2bd383985bb2c2bb6bbb7c98b9b5cf30192e
Taran <thetaranneeld@gmail.com>
2019-10-04 11:07:14 -0600

    added get and post actions requests/added validateAction middleware

--- data/projectRouter.js
+++ data/projectRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const Projects = require('./helpers/projectModel.js');
+const Actions = require('./helpers/actionModel.js');
 
 const router = express.Router();
 
@@ -8,6 +9,17 @@ router.get('/', (req, res) => {
     Projects.get()
         .then(project => {
             res.status(200).json(project);
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'failed to get projects from database' });
+        });
+});
+
+router.get('/:id/actions', validateProjectId, (req, res) => {
+    const id = req.params.id;
+    Projects.getProjectActions(id)
+        .then(actions => {
+            res.status(200).json(actions);
         })
         .catch(err => {
             res.status(500).json({ error: 'failed to get projects from database' });
@@ -22,6 +34,18 @@ router.post('/', validateProject, (req, res) => {
         })
         .catch(err => {
             res.status(500).json({ error: 'failed to post project to the database' });
+        });
+});
+
+router.post('/:id/actions', validateProjectId, validateAction, (req, res) => {
+    const id = req.params.id;
+    const actionData = req.body;
+    Actions.insert(actionData)
+        .then(actions => {
+            res.status(200).json(actions);
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'failed to post action to database' });
         });
 });
 
@@ -72,6 +96,25 @@ function validateProject(req, res, next) {
         res.status(400).json({ error: 'missing project description field' });
     } else {
         console.log('project validated');
+        next();
+    }
+};
+
+function validateAction(req, res, next) {
+    const actionData = req.body;
+    if (!actionData.project_id) {
+        res.status(400).json({ message: 'missing project id field' });
+    } else if (!actionData.description) {
+        res.status(400).json({ message: 'missing action description field' });
+    } else if (!actionData.notes) {
+        res.status(400).json({ message: 'missing action notes field' });
+    } else {
+        if (JSON.stringify(actionData.description).length > 128) {
+            console.log('maximum description character limit reached');
+            const desc = JSON.stringify(actionData.description).substring(0, 127);
+            actionData.description = desc.substring(1, desc.length);
+        }
+        console.log('action validated');
         next();
     }
 };
