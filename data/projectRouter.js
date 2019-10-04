c7f4ccb019b6e02c7509174c2269fc3c1def919e
Taran <thetaranneeld@gmail.com>
2019-10-04 10:35:15 -0600

    added delete and put requests

--- data/projectRouter.js
+++ data/projectRouter.js
@@ -18,10 +18,34 @@ router.post('/', validateProject, (req, res) => {
     const body = req.body;
     Projects.insert(body)
         .then(project => {
-            res.status(200).json(project);
+            res.status(201).json(project);
         })
         .catch(err => {
             res.status(500).json({ error: 'failed to post project to the database' });
+        });
+});
+
+router.delete('/:id', validateProjectId, (req, res) => {
+    const id = req.params.id;
+    Projects.remove(id)
+        .then(project => {
+            res.status(200).json({ message: 'project removed' });
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'failed to remove project from the database' });
+        });
+
+});
+
+router.put('/:id', validateProjectId, validateProject, (req, res) => {
+    const id = req.params.id;
+    const changes = req.body;
+    Projects.update(id, changes)
+        .then(project => {
+            res.status(200).json(changes);
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'failed to update project' });
         });
 });
 
@@ -29,13 +53,12 @@ router.post('/', validateProject, (req, res) => {
 
 function validateProjectId(req, res, next) {
     const id = req.params.id;
-    Projects.getById(id)
+    Projects.get(id)
         .then(project => {
             if (!id) {
-                res.status(400).json({ message: 'invalid project id' });
+                res.status(400).json({ error: 'invalid project id' });
             } else {
                 console.log('project id validated');
-                res.status(200).json(project);
                 next();
             }
         })
@@ -44,9 +67,9 @@ function validateProjectId(req, res, next) {
 function validateProject(req, res, next) {
     const projectData = req.body;
     if (!projectData.name) {
-        res.status(400).json({ message: 'missing project name field' });
+        res.status(400).json({ error: 'missing project name field' });
     } else if (!projectData.description) {
-        res.status(400).json({ message: 'missing project description field' });
+        res.status(400).json({ error: 'missing project description field' });
     } else {
         console.log('project validated');
         next();
